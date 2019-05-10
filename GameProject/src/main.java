478a23c6255572ad3e2a4db26028122bde255eab
Anıl Şenay <anilsenay3@gmail.com>
2019-05-10 22:32:50 +0300

    undo last change

--- GameProject/src/main.java
+++ GameProject/src/main.java
@@ -101,10 +101,12 @@ public class main extends Application {
         primaryStage.show();
         // Main scene end //
         
+        lvlStage = new GameStage(); //create new gameStage class to start game
+        
         // ** button actions **//
         //start button action
         startButton.setOnMouseClicked(e -> {
-            lvlStage = new GameStage(); //create new gameStage class to start game
+
             main.buttonPlay(); //button click sound effect
             //After click the start button it starts the game with fade effect.
             FadeTransition fade = new FadeTransition();
